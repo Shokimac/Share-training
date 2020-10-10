6698280b0da9b9e0a3216bebbf748ed719ace29f
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-10 19:12:50 +0900

    [Add]_bookmarks_column_to_migrate_file

--- db/migrate/20201010100139_create_bookmarks.rb
+++ db/migrate/20201010100139_create_bookmarks.rb
@@ -1,7 +1,8 @@
 class CreateBookmarks < ActiveRecord::Migration[5.2]
   def change
     create_table :bookmarks do |t|
-
+      t.integer :user_id, default: 0, null: false
+      t.integer :team_id, default: 0, null: false
       t.timestamps
     end
   end
