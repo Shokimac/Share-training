9466cb292da0ad509e5bb240b6c6369c3b9cee64
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-10 19:21:29 +0900

    [Add]_TrainingRecords_column_to_migrate_file

--- db/migrate/20201010100444_create_training_records.rb
+++ db/migrate/20201010100444_create_training_records.rb
@@ -1,7 +1,10 @@
 class CreateTrainingRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :training_records do |t|
-
+      t.integer :user_id, default: 0, null: false
+      t.string :event
+      t.string :body
+      t.text :comment
       t.timestamps
     end
   end
