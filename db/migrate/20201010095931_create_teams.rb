bc88119f7d2cc54e86ab70a7f7e66f45b2dc8ae8
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-10 19:10:30 +0900

    [Add]_teams_column_to_migrate_file

--- db/migrate/20201010095931_create_teams.rb
+++ db/migrate/20201010095931_create_teams.rb
@@ -1,7 +1,10 @@
 class CreateTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
-
+      t.string :name
+      t.text :introduction
+      t.integer :genre_id, default: 0, null: false
+      t.string  :image_id
       t.timestamps
     end
   end
