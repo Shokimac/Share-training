364bf672a1f52287dc9c1308795ca421b8164dbd
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-10 19:15:38 +0900

    [Add]_footprint_column_to_migrate_file

--- db/migrate/20201010100210_create_footprints.rb
+++ db/migrate/20201010100210_create_footprints.rb
@@ -1,7 +1,8 @@
 class CreateFootprints < ActiveRecord::Migration[5.2]
   def change
     create_table :footprints do |t|
-
+      t.integer :user_id, default: 0, null: false
+      t.integer :team_id, default: 0, null: false
       t.timestamps
     end
   end
