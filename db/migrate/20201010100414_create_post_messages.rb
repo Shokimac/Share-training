76dc4d9b19ea0f9c75ebd2ae85646b3846137538
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-10 19:19:31 +0900

    [Add]_PostMessages_column_to_migrate_file

--- db/migrate/20201010100414_create_post_messages.rb
+++ db/migrate/20201010100414_create_post_messages.rb
@@ -1,7 +1,9 @@
 class CreatePostMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :post_messages do |t|
-
+      t.integer :user_id, default: 0, null: false
+      t.integer :team_id, default: 0, null: false
+      t.text :body
       t.timestamps
     end
   end
