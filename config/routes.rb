3ebaf1532474107d3193b155dda646504caeaa0f
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-10 22:34:24 +0900

    Make_teams_new_show_edit_index_action_and_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'teams/new'
+  get 'teams/show'
+  get 'teams/edit'
+  get 'teams/index'
   get 'users/edit'
   get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
