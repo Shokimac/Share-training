7cb2dd7a2d40db2de6b5727749a9674304d5009a
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-10 18:53:33 +0900

    [Add]_user_column_to_migrate_file

--- db/migrate/20201010094507_create_users.rb
+++ db/migrate/20201010094507_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string  :name,               null: false, default: ""
+      t.string  :email
+      t.string  :goal
+      t.string  :password
+      t.string  :image_id
       t.timestamps
     end
   end
