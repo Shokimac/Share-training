bd336efc16a3ac72898cb2f130f2df27615d929e
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-11 18:02:45 +0900

    [Add]_user_signup_with_goal_column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     private
 
     def configure_permitted_parameters
-        added_attrs = [:email, :name, :password, :password_confirmation, :image, :genre_id]
+        added_attrs = [:email, :name, :password, :password_confirmation, :image, :genre_id, :goal]
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
         devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
