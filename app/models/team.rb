fe0be8e504548e16b9cbc16c71da6be62c642fb7
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-11 17:08:32 +0900

    [Add]_refile_image

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
+    attachment :image
     has_many :bookmarks, dependent: :destroy
     has_many :footprints, dependent: :destroy
     has_many :team_members, dependent: :destroy
     has_many :post_messages, dependent: :destroy
-    belongs_to :genre
 end
