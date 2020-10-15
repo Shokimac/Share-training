7b47081da91aa95040684a4cb802867671acb2d7
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-15 16:29:59 +0900

    [Add]_bookmark_create_action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,13 @@
 class BookmarksController < ApplicationController
 
     def create
-
+        @team = Team.find(params[:team_id])
+        bookmark = Bookmark.new(user_id: current_user.id, team_id: @team.id)
+        bookmark.save
+        redirect_to team_path(@team)
     end
 
     def destroy
 
     end
-    
 end
