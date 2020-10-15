4e3a9141cc5a534ef20bc6e8750b786e55edb250
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-15 16:13:49 +0900

    [Fix]_bookmark_routing

--- config/routes.rb
+++ config/routes.rb
@@ -15,14 +15,15 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update] do
     resources :post_messages, only: [:create, :destroy]
-    resources :bookmarks, only: [:create, :destroy]
     resources :training_records, only: [:new, :create, :edit, :update, :destroy]
     get 'confirmation' => 'users#confirmation'
     post 'withdrawal' => 'users#withdrawal'
     patch 'withdrawal' => 'users#withdrawal'
   end
   
-  resources :teams
+  resources :teams do
+    resources :bookmarks, only: [:create, :destroy]
+  end
   resource  :inquiries, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
