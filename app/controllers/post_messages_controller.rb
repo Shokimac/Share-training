95dd7745f199a5b9130874dba8f590b5093a3ac1
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-18 19:07:33 +0900

    [Add]_post_message_create_action #14

--- app/controllers/post_messages_controller.rb
+++ app/controllers/post_messages_controller.rb
@@ -1,9 +1,18 @@
 class PostMessagesController < ApplicationController
     def create
-
+        message = PostMessage.new(message_params)
+        message.user_id = current_user.id
+        message.team_id = params[:team_id]
+        message.save
+        redirect_to team_path(params[:team_id])
     end
 
-    def destoy
-        
+    def destroy
+    end
+
+    private
+
+    def message_params
+        params.require(:post_message).permit(:body)
     end
 end
