c7bac4862d241bc18afaed84544b624575863791
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-21 23:11:34 +0900

    [Add]_training_records_edit_&_update_action #18

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -10,11 +10,17 @@ class TrainingRecordsController < ApplicationController
     end
 
     def edit
-
+        @record = TrainingRecord.find(params[:id])
+        @genres = TrainingGenre.where(user_id: current_user.id).all
     end
 
     def update
-
+        record = TrainingRecord.find(params[:id])
+        record.training_genre_id = params[:event]
+        binding.pry
+        record.update(record_params)
+        binding.pry
+        redirect_to user_path(current_user)
     end
 
     def destroy
