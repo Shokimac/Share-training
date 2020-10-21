8cdf3a469011b68bdf698130753f4d35d6302b16
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-21 18:16:52 +0900

    [Add]_training_records_create_action #16

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -1,7 +1,12 @@
 class TrainingRecordsController < ApplicationController
 
     def create
-
+        record = TrainingRecord.new(record_params)
+        record.user_id = current_user.id
+        record.training_genre_id = params[:event]
+        record.save
+        binding.pry
+        redirect_to user_path(params[:user_id])
     end
 
     def edit
@@ -14,5 +19,11 @@ class TrainingRecordsController < ApplicationController
 
     def destroy
 
+    end
+
+    private
+
+    def record_params
+        params.require(:training_record).permit(:body, :comment)
     end
 end
