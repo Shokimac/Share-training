0f8bed6259c86be1901f40c160ad7b64ceacaa93
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-26 15:52:58 +0900

    [Add]_bookmark_ranking_into_instance #21

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
     @newTeams = Team.order(:created_at).limit(5)
+    @bookmarks = Team.limit(5).sort {|a,b| b.bookmarks.count <=> a.bookmarks.count}
   end
 
   def search
