b37d7dcd82c2dd4201fcd78091a673b7e0f383f9
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-27 21:12:22 +0900

    [Add]_team_slider_on_Top_view #21

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,24 +1,53 @@
-$(function () {
-    let mySwiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        // direction: 'vertical',
-        loop: true,
-        effect: 'coverflow',
+$(document.addEventListener('DOMContentLoaded', function () {
+    const hero = new HeroSlider('.swiper-container');
+    hero.start();
+}));
 
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
+class HeroSlider {
+    constructor(el) {
+        this.el = el;
+        this.swiper = this._initSwiper();
+    }
 
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+    _initSwiper() {
+        return this.swiper = new Swiper(this.el, {
+            // Optional parameters
+            // direction: 'vertical',
+            loop: true,
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 1,
+            speed: 1000,
+            // breakpoints: {
+            //     1024: {
+            //         slidesPerView: 2,
+            //     }
+            // },
+            
+            // If we need pagination
+            // pagination: {
+            //     el: '.swiper-pagination',
+            // },
+    
+            // // Navigation arrows
+            // navigation: {
+            //     nextEl: '.swiper-button-next',
+            //     prevEl: '.swiper-button-prev',
+            // },
+    
+            // // And if we need scrollbar
+            // scrollbar: {
+            //     el: '.swiper-scrollbar',
+            // },
+        })
+    }
 
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    })
-});
+    start() {
+        this.swiper.params.autoplay = {
+            delay: 4000,
+            disableOnInteraction: false,
+        }
+        this.swiper.autoplay.start();
+    }
+}
